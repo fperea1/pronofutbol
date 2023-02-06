95348f98f901f87440f310def946f4e2496c54fd
Francisco C. <fran.perea.74@gmail.com>
2023-02-06 20:05:56 +0100

    Prueblas conflictos

--- src/main/java/com/fcpm/pronofutbol/constant/Constantes.java
+++ src/main/java/com/fcpm/pronofutbol/constant/Constantes.java
@@ -6,6 +6,8 @@ public final class Constantes {
 	
     // mapeo controller
 	
+    // mapeo controller 2
+	
 	public static final String AUTENTICATION = "/autenticacion";
 	
 	public static final String CONTACTO = "/contacto";
